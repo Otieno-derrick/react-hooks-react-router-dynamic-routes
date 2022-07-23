1f8f36f482ac301044636c8a35374f219db07364
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-07-23 08:39:19 +0300

    done

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 function MovieShow({movies}) {
+    const params = useParams();
   return (
     <div>
       <h3>{movies[params.movieId].title}</h3>
